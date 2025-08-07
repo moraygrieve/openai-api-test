a6bab8599cdca4b64f18bf539e7e6657e4401892
Moray Grieve <moray.grieve@me.com>
2025-08-07 07:32:22 +0100

    Default to 4 spaces

--- test/openai_cor_001/run.py
+++ test/openai_cor_001/run.py
@@ -3,24 +3,24 @@ from pysys.constants import *
 from pysys.basetest import BaseTest
 
 class PySysTest(BaseTest):
-	def execute(self):
+    def execute(self):
 
-		# create the client and using gpt-4.1 request a response
-		client = OpenAI()
-	
-		input_messages = [
-        	{"role": "system", "content": "You are a character from Hitchhiker's Guide to the Galaxy."},
-        	{"role": "user", "content": "What's the answer to the universe? Just give the number, no other characters."}
-    	]
+        # create the client and using gpt-4.1 request a response
+        client = OpenAI()
+    
+        input_messages = [
+            {"role": "system", "content": "You are a character from Hitchhiker's Guide to the Galaxy."},
+            {"role": "user", "content": "What's the answer to the universe? Just give the number, no other characters."}
+        ]
 
-		response = client.responses.create(
+        response = client.responses.create(
             model="gpt-4.1",
             input=input_messages
         )
     
-		# log out the response
-		self.log.info('The answer to the universe is %s', response.output_text)
+        # log out the response
+        self.log.info('The answer to the universe is %s', response.output_text)
 
-		# assert that the response is not empty
-		self.assertTrue(int(response.output_text) == 42, assertMessage = "Assert the answer is correct")
+        # assert that the response is not empty
+        self.assertTrue(int(response.output_text) == 42, assertMessage = "Assert the answer is correct")
     
