ceaf2f713d014a9d796195b62fe7019028966050
Moray Grieve <moray.grieve@me.com>
2025-08-07 07:34:57 +0100

    Updated for settings

--- test/openai_cor_001/run.py
+++ test/openai_cor_001/run.py
@@ -7,7 +7,7 @@ class PySysTest(BaseTest):
 
         # create the client and using gpt-4.1 request a response
         client = OpenAI()
-    
+
         input_messages = [
             {"role": "system", "content": "You are a character from Hitchhiker's Guide to the Galaxy."},
             {"role": "user", "content": "What's the answer to the universe? Just give the number, no other characters."}
@@ -17,10 +17,10 @@ class PySysTest(BaseTest):
             model="gpt-4.1",
             input=input_messages
         )
-    
+
         # log out the response
         self.log.info('The answer to the universe is %s', response.output_text)
 
         # assert that the response is not empty
         self.assertTrue(int(response.output_text) == 42, assertMessage = "Assert the answer is correct")
-    
+
