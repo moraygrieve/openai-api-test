016ba84313096ce69017382530fe25159f88b9b4
Moray Grieve <moray.grieve@me.com>
2025-08-14 09:14:20 +0100

    Add asyncronous test

--- src/utils/docker.py
+++ src/utils/docker.py
@@ -1,6 +1,11 @@
 import docker, select, time
+import asyncio, threading
 
 class DockerShell:
+
+    def __init__(self, image="ubuntu"):
+        """Initialize a docker container with a non-interactive shell."""
+        self.client = docker.from_env()
 
     def wait_container(self, container, timeout=10):
         """Wait for a container to be running."""
@@ -12,12 +17,23 @@ class DockerShell:
             time.sleep(0.5)
             container.reload()
 
+    def close(self):
+        """Close a running container cleanly."""
+        try:
+            self.container.stop(timeout=1)
+            self.container.remove(force=True)
+        except Exception:
+            pass
+
+    def __del__(self):
+        """Ensure container is closed cleanly on object deletion."""
+        self.close()
 
 class DockerNonInteractiveShell(DockerShell):
 
     def __init__(self, image="ubuntu"):
         """Initialize a docker container with a non-interactive shell."""
-        self.client = docker.from_env()
+        super().__init__(image)
         self.container = self.client.containers.run(image, command="bash", tty=False, stdin_open=True, detach=True)
 
     def run(self, command):
@@ -43,7 +59,7 @@ class DockerInteractiveShell(DockerShell):
 
     def __init__(self, image="ubuntu"):
         """ Initialize a Docker container with an interactive shell session."""
-        self.client = docker.from_env()
+        super().__init__(image)
         self.container = self.client.containers.run(image, command=["bash", "-c", "export PS1=''; exec sh"],
             tty=False, stdin_open=True, detach=True, )
         self.sock = self.container.attach_socket(params={"stdin": 1, "stdout": 1, "stream": 1})
@@ -86,6 +102,34 @@ class DockerInteractiveShell(DockerShell):
         except Exception:
             pass
 
-    def __del__(self):
-        """Ensure container is closed cleanly on object deletion."""
-        self.close()
+class DockerAsynchronousShell(DockerShell):
+
+    def __init__(self, image="ubuntu"):
+        """Initialize a docker container with a non-interactive shell."""
+        super().__init__(image)
+        self._loop = asyncio.new_event_loop()
+        self._thread = threading.Thread(target=self._run_loop, daemon=True)
+        self._thread.start()
+        self._future = None
+        self.container = self.client.containers.run(image, command="bash", tty=False, stdin_open=True, detach=True)
+
+    def run(self, command, timeout=5):
+        if self._future is None or self._future.done():
+            self._future = asyncio.run_coroutine_threadsafe(self._run_command(command), self._loop)
+
+    def poll_done(self):
+        return self._future is not None and self._future.done()
+
+    def get_result(self):
+        if self.poll_done():
+            return self._future.result()
+        return None
+
+    async def _run_command(self, command):
+        result = self.container.exec_run("bash -c \'%s\'" % command)
+        result = result.output.decode().strip()
+        return result.splitlines()
+
+    def _run_loop(self):
+        asyncio.set_event_loop(self._loop)
+        self._loop.run_forever()
---
+++ test/docker_003/run.py
@@ -0,0 +1,23 @@
+import time
+from pysys.constants import TIMEDOUT
+from pysys.basetest import BaseTest
+from utils.docker import DockerAsynchronousShell
+
+class PySysTest(BaseTest):
+    def execute(self):
+        shell = DockerAsynchronousShell()
+        shell.run("sleep 10; echo hello world")
+
+        start = time.time()
+        while shell.poll_done() == False:
+            self.log.info('Command has not yet completed ...')
+            time.sleep(1)
+            if (time.time() - start) > 20:
+                self.addOutcome(TIMEDOUT, outcomeReason='Timedout waiting for docker response', abortOnError=True)
+        result = shell.get_result()
+        self.log.info('Result obtained: %s' % result)
+
+        self.assertTrue(result[0]== 'hello world', assertMessage='Simple echo command')
+        shell.close()
+
+
